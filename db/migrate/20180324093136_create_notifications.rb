c122a19ee1e614954446f2df8feb125bb4cb8187
Muhammad Taimoor Sultani <sultani4work@gmail.com>
2018-03-24 21:25:17 +0500

    Notification Category Changed to Integer

--- db/migrate/20180324093136_create_notifications.rb
+++ db/migrate/20180324093136_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[5.1]
   def change
     create_table :notifications do |t|
       t.text :description
-      t.string :category
+      t.integer :category
       t.boolean :read, :default => false
       t.references :user, foreign_key: true
       t.references :product, foreign_key: true
