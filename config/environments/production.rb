3f0f0a48d07f244abb711bfcb93e6f3376a48c6f
Muhammad Taimoor Sultani <sultani4work@gmail.com>
2018-04-12 21:50:54 +0500

    HTTPS Host added in Enviorment.rb and Focus Removed from Subscribe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Mailer Configurations Code
-  config.action_mailer.default_url_options = {:host => 'dev-traffle.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
+  config.action_mailer.default_url_options = {:host => 'dev-traffle.herokuapp.com', :protocol => 'https'} #I've also tried it without ":protocol => 'http'"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
